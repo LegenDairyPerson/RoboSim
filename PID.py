67b86102891faae7302d7fac1ad075005eeea31a
LegenDairyPerson <68968417+LegenDairyPerson@users.noreply.github.com>
2023-08-11 15:15:25 -0400

    Update PID.py

--- PID.py
+++ PID.py
@@ -6,6 +6,9 @@ from decimal import Decimal
 
 class PID:
 
+    lefti = 0
+    righti = 0
+
     def __init__(self, lrpm, rrpm, targets, car):
         self.left = car.roundedLeft
         self.right = car.roundedRight
@@ -19,10 +22,13 @@ class PID:
 
         self.p = 0.01
         self.i = 0
-        self.d = 0.5
+        self.d = 0
 
         self.lefti = 0
         self.righti = 0
+
+        self.rsense = 1.1
+        self.lsense = 1
 
     def update(self, lrpm, rrpm, targets, car=None):
         if car is None:
@@ -36,25 +42,40 @@ class PID:
         self.rrpm = rrpm
         self.targets = targets
 
-        return self.get_new_rpms()
+        return self.get_new_rpms
 
+    @property
     def get_new_rpms(self):
 
-        left_y_dist = abs(self.targets[1] - self.left[1])
-        right_y_dist = abs(self.targets[2] - self.right[1])
+        left_y_dist = self.targets[1] - self.left[1]
+        right_y_dist = self.targets[2] - self.right[1]
 
-        leftp = self.p * left_y_dist
-        lefti = self.i * 0
+        if left_y_dist < 0:  # buggy is below
+            rsense = max(self.rsense, self.lsense)
+            lsense = min(self.rsense, self.lsense)
+            self.rsense = rsense
+            self.lsense = lsense
+        elif left_y_dist > 0:  # buggy is above
+            lsense = max(self.rsense, self.lsense)
+            rsense = min(self.rsense, self.lsense)
+            self.lsense = lsense
+            self.rsense = rsense
+
+        leftp = self.p * abs(left_y_dist) * self.lsense
+        lefti = self.i * (PID.lefti * 0)
         leftd = self.d * (self.lrpm - self.last_lrpm)
-        leftsum = float(leftp) + float(lefti) + float(leftd)
+        leftsum = 0 if abs(left_y_dist) < 0.5 else float(leftp) + float(lefti) + float(leftd)
+        # leftsum = float(leftp) + float(lefti) + float(leftd)
         self.last_lrpm = self.lrpm
 
-        rightp = self.p * right_y_dist * 2
-        righti = self.i * 0
+        rightp = self.p * abs(right_y_dist) * self.rsense
+        righti = self.i * (PID.righti * 0)
         rightd = self.d * (self.rrpm - self.last_rrpm)
-        rightsum = float(rightp) + float(righti) + float(rightd)
+        rightsum = 0 if abs(right_y_dist) < 0.5 else float(rightp) + float(righti) + float(rightd)
+        # rightsum = float(rightp) + float(righti) + float(rightd)
         self.last_rrpm = self.rrpm
 
+        print([self.lsense, self.rsense])
         new_left = round(leftsum, 6)
         new_right = round(rightsum, 6)
 
